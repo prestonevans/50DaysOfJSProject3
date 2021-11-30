55b50f86fea1e8ac44c44333e547f8ded1bef062
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-11-30 11:17:43 -0700

    my attempt to recreate project before watching video

--- js/app.js
+++ js/app.js
@@ -1,2 +1,7 @@
 //document.body.style.backgroundColor = "red";
 //test me
+const toggle = document.querySelector('.button');
+const content = document.querySelector('.content')
+toggle.addEventListener('click', () => {
+    content.classList.toggle('tilt')
+})
